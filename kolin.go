e3d22b704680e49dc3df657f0e18beeecdfe5416
Kian van der Meer <kiansinan@gmail.com>
2022-02-27 17:10:53 +0100

    basic connection

--- kolin.go
+++ kolin.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello marta.")
+
+	CONNECT := "localhost:2222"
+
+	conn, err := net.Dial("tcp", CONNECT)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print(">> ")
+
+		text, _ := reader.ReadString('\n')
+
+		fmt.Fprintf(conn, text+"\n")
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+		fmt.Print("->: " + message)
+
+		if strings.TrimSpace(string(text)) == "STOP" {
+			fmt.Println("TCP client exiting...")
+			return
+		}
+	}
 }
