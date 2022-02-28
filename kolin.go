85fde4ec23aa2bb12ff66ea72700894221954195
Kian van der Meer <kiansinan@gmail.com>
2022-02-28 13:04:15 +0100

    idk

--- kolin.go
+++ kolin.go
@@ -1,3 +1,5 @@
+// client
+
 package main
 
 import (
@@ -6,35 +8,88 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
+
+const Port = 2222
+const cmdPrefix = "&"
+
+var conn net.Conn
 
 func main() {
 
-	CONNECT := "localhost:2222"
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", Port))
 
-	conn, err := net.Dial("tcp", CONNECT)
-
+	// check if initiating the listener failed
 	if err != nil {
+		fmt.Println("Failed initiating the listener.")
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
+	// defer .Close()
+	// it will be executed at the end of the main function
+	defer listener.Close()
+
+	// main loop to check of incoming connections
 	for {
-		reader := bufio.NewReader(os.Stdin)
 
-		fmt.Print(">> ")
+		// accepting an incoming connection
+		conn, err = listener.Accept()
 
-		text, _ := reader.ReadString('\n')
-
-		fmt.Fprintf(conn, text+"\n")
-
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-
-		fmt.Print("->: " + message)
-
-		if strings.TrimSpace(string(text)) == "STOP" {
-			fmt.Println("TCP client exiting...")
-			return
+		if err != nil {
+			fmt.Println("Failed accepting a connection.")
+			fmt.Println(err)
 		}
+
+		// read stream until \n
+		netData, err := bufio.NewReader(conn).ReadString('\n')
+
+		// if marta is ofline, a eof error will be thrown
+		if err != nil {
+			// wait 5 seconds and try to reconnect
+			fmt.Println("Marta is offline. Trying to reconnect in 5 seconds.")
+			time.Sleep(5 * time.Second)
+		} else {
+			// handle data
+			handleData(netData)
+		}
+
+		// close connection
+		conn.Close()
+	}
+}
+
+func handleData(rawStr string) {
+	// print rawStr without new line at the end
+	fmt.Println("Received: '" + string(rawStr)[0:len(rawStr)-1] + "'")
+
+	// if data is a command
+	if string(rawStr[0]) == cmdPrefix {
+
+		// remove first char and split rawStr after every " "
+		data := strings.Split(rawStr[1:len(rawStr)-1], " ")
+		cmd := data[0]
+
+		fmt.Println("'" + cmd + "'")
+
+		switch cmd {
+		case "ping":
+			handlePing()
+		}
+	}
+}
+
+func handlePing() {
+	fmt.Println("Got pinged.")
+	sendToConn("Pong.")
+}
+
+func sendToConn(str string) {
+	_, err := conn.Write([]byte(str + "\n"))
+
+	if err != nil {
+		fmt.Println("Failed responding to ping command.")
+		fmt.Println(err)
 	}
 }
