e5de6c53d62a94f7f348e3720e61597bde2e7426
Kian van der Meer <kiansinan@gmail.com>
2022-02-28 21:01:29 +0100

    ground

--- kolin.go
+++ kolin.go
@@ -3,95 +3,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
-	"os"
-	"strings"
 	"time"
+
+	"github.com/karastift/marta-client/world"
 )
 
-const Port = 2222
-const cmdPrefix = "&"
+const marta = "127.0.0.1"
+const port = 2222
 
-var conn net.Conn
+var server *world.Server
 
 func main() {
+	server = world.NewServer(marta, port)
 
-	// os.Exit(1)
-
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", Port))
-
-	// check if initiating the listener failed
-	if err != nil {
-		fmt.Println("Failed initiating the listener.")
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// defer .Close()
-	// it will be executed at the end of the main function
-	defer listener.Close()
-
-	// main loop to check of incoming connections
-	for {
-
-		// accepting an incoming connection
-		conn, err = listener.Accept()
-
-		if err != nil {
-			fmt.Println("Failed accepting a connection.")
-			fmt.Println(err)
-		}
-
-		// read stream until \n
-		netData, err := bufio.NewReader(conn).ReadString('\n')
-
-		// if marta is ofline, a eof error will be thrown
-		if err != nil {
-			// wait 5 seconds and try to reconnect
-			fmt.Println("Marta is offline. Trying to reconnect in 5 seconds.")
-			time.Sleep(5 * time.Second)
-		} else {
-			// handle data
-			handleData(netData)
-		}
-
-		// close connection
-		conn.Close()
-	}
-}
-
-func handleData(rawStr string) {
-	// print rawStr without new line at the end
-	fmt.Println("Received: '" + string(rawStr)[0:len(rawStr)-1] + "'")
-
-	// if data is a command
-	if string(rawStr[0]) == cmdPrefix {
-
-		// remove first char and split rawStr after every " "
-		data := strings.Split(rawStr[1:len(rawStr)-1], " ")
-		cmd := data[0]
-
-		fmt.Println("'" + cmd + "'")
-
-		switch cmd {
-		case "ping":
-			handlePing()
-		}
-	}
-}
-
-func handlePing() {
-	fmt.Println("Got pinged.")
-	sendToConn("Pong.")
-}
-
-func sendToConn(str string) {
-	_, err := conn.Write([]byte(str + "\n"))
+	_, err := server.Connect()
 
 	if err != nil {
-		fmt.Println("Failed responding to ping command.")
-		fmt.Println(err)
+		fmt.Println("Marta is offline.")
+		return
 	}
+
+	server.Listen(handleData)
+}
+
+func handleData(data string) {
+	fmt.Println("[" + time.Now().Format(time.ANSIC) + "]" + " Received: '" + data[:len(data)-1] + "'")
+
+	server.Conn.Write([]byte("Beat me out of me\n"))
+
+	fmt.Println("Wrote")
 }
---
+++ world/server.go
@@ -0,0 +1,83 @@
+package world
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
+
+type Server struct {
+	Addr           string
+	Port           int
+	Conn           net.Conn
+	PauseListening bool
+}
+
+func NewServer(addr string, port int) *Server {
+	return &Server{
+		Addr:           addr,
+		Port:           port,
+		PauseListening: false,
+	}
+}
+
+func (server *Server) Send(data []byte) ([]byte, error) {
+
+	server.Conn.Write(data)
+
+	responseData, err := bufio.NewReader(server.Conn).ReadBytes('\n')
+
+	return []byte(responseData), err
+}
+
+func (server *Server) Listen(handleData func(data string)) {
+	for {
+		if server.PauseListening {
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		data, err := bufio.NewReader(server.Conn).ReadString('\n')
+
+		if err != nil {
+			fmt.Println("Marta is offline.")
+			break
+		}
+
+		go handleData(data)
+	}
+}
+
+func (server *Server) IsConnected() bool {
+	return server.Conn != nil
+}
+
+func (server *Server) Connect() (net.Conn, error) {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", server.Addr, server.Port))
+
+	if err != nil {
+		return nil, err
+	}
+	server.Conn = conn
+
+	response, err := server.Send([]byte("marta login\n"))
+
+	if err != nil {
+		fmt.Println("Login failed.")
+	}
+
+	if checkResponse(string(response)) {
+		fmt.Println("[" + time.Now().Format(time.ANSIC) + "] Successfully connected to marta.")
+
+	}
+
+	return conn, err
+}
+
+func (server *Server) Disconnect() {
+	server.Conn.Close()
+}
+
+func checkResponse(res string) bool {
+	return res == "marta logged in\n"
+}
